8b7bce1cafb2ce6cdf649494b1c03c79fe761c3f
Youll8 <888youll@gmail.com>
2021-03-31 00:46:52 +0300

    feat: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+    if (!Array.isArray(arr)) {
+  		return false;
+  	}
+
+  	let team = [];
+  	for (let elem of arr) {
+      if (typeof elem !== 'string') {
+        continue;
+      }
+
+      const chr = elem.trim().substr(0, 1).toUpperCase();
+      if (!/^[A-Z]{1}$/.test(chr)) {
+        continue;
+      }
+
+  		team.push(chr);
+
+  	}
+  	return team.sort().join('');
 };
